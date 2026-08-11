2856bb7c14e28d3533bd8e53521fc790479eda0a
Miguel Cardenas <mifica12@gmail.com>
2026-08-11 17:04:40 -0600

    feat: Home consume API de publicaciones

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,39 @@
+import { useEffect, useState } from "react";
+import api from "../services/api";
+
 const Home = () => {
-  return <h1>Home</h1>;
+  const [publicaciones, setPublicaciones] = useState([]);
+  const [cargando, setCargando] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const cargarPublicaciones = async () => {
+      try {
+        const respuesta = await api.get("/publicaciones");
+        // console.log(respuesta);
+        setPublicaciones(respuesta.data);
+      } catch (error) {
+        setError("error al cargar publicaciones");
+      } finally {
+        setCargando(false);
+      }
+    };
+
+    cargarPublicaciones();
+  }, []);
+  return (
+    <div>
+      {cargando && <p>Cargando...</p>}
+      {error && <p>{error}</p>}
+      {!cargando && !error && (
+        <ul>
+          {publicaciones.map((pub) => (
+            <li key={pub.id}>{pub.titulo}</li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
 };
 
 export default Home;
